7d074bbb6e51c2d03cfd1b481279acbd1e4d2c10
wuzh <276229118@qq.com>
2021-04-14 00:49:38 +0800

    Use custom file provider

---
+++ library/src/main/java/com/wuadam/awesomewebview/helpers/FileProvider4WebView.java
@@ -0,0 +1,9 @@
+package com.wuadam.awesomewebview.helpers;
+
+
+import android.support.annotation.Keep;
+import android.support.v4.content.FileProvider;
+
+@Keep
+public final class FileProvider4WebView extends FileProvider {
+}
